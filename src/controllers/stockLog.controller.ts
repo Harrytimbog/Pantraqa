29eebe376a62558496bfad468ac8d477590e83f7
Timilehin Arigbede <harriemannie@gmail.com>
2025-04-15 13:30:09 +0100

    Made Stock Log Filterable for better Auditing

--- src/controllers/stockLog.controller.ts
+++ src/controllers/stockLog.controller.ts
@@ -5,10 +5,35 @@ import Drink from '../models/drink.model';
 import User from '../models/user.model';
 import StorageLocation from '../models/storageLocation.model';
 import { logger } from '../utils/logger';
+import { Op } from 'sequelize';
 
 export const getAllStockLogs = async (req: Request, res: Response): Promise<void> => {
     try {
+        const {
+            userId,
+            drinkId,
+            storageLocationId,
+            action,
+            dateFrom,
+            dateTo
+        } = req.query;
+
+        const filters: any = {};
+
+        if (userId) filters.userId = userId;
+        if (drinkId) filters.drinkId = drinkId;
+        if (storageLocationId) filters.storageLocationId = storageLocationId;
+        if (action) filters.action = action;
+
+        if (dateFrom || dateTo) {
+            filters.createdAt = {
+                ...(dateFrom && { [Op.gte]: new Date(dateFrom as string) }),
+                ...(dateTo && { [Op.lte]: new Date(dateTo as string) })
+            };
+        }
+
         const logs = await StockLog.findAll({
+            where: filters,
             include: [
                 { model: User, attributes: ['id', 'email', 'role'] },
                 { model: Drink, attributes: ['id', 'name', 'size', 'category'] },
