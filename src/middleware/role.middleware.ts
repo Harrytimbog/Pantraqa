8647784fa66727c0dbda076b6373868772fa10d6
Timilehin Arigbede <harriemannie@gmail.com>
2025-04-22 23:36:36 +0100

    .

--- src/middleware/role.middleware.ts
+++ src/middleware/role.middleware.ts
@@ -1,29 +1,28 @@
-import { Response, NextFunction } from 'express';
-import { StatusCodes } from 'http-status-codes';
-import { AuthRequest } from '../../types/express';
+import { Response, NextFunction } from "express";
+import { StatusCodes } from "http-status-codes";
+import { AuthRequest } from "../../types/express";
 
 export const onlyManager = (
-    req: AuthRequest,
-    res: Response,
-    next: NextFunction
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
 ): void => {
-    if (req.user?.role === 'manager' || req.user?.role === 'admin') {
-        next();
-    } else {
-        res.status(StatusCodes.FORBIDDEN).json({ message: 'Access denied' });
-    }
+  if (req.user?.role === "manager" || req.user?.role === "admin") {
+    next();
+  } else {
+    res.status(StatusCodes.FORBIDDEN).json({ message: "Access denied" });
+  }
 };
 
-
 export const onlyAdmin = (
-    req: AuthRequest,
-    res: Response,
-    next: NextFunction
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
 ): void => {
-    console.log(req)
-    if (req.user?.role === 'admin') {
-        next();
-    } else {
-        res.status(StatusCodes.FORBIDDEN).json({ message: 'Access denied' });
-    }
-}
+  // console.log(req)
+  if (req.user?.role === "admin") {
+    next();
+  } else {
+    res.status(StatusCodes.FORBIDDEN).json({ message: "Access denied" });
+  }
+};
